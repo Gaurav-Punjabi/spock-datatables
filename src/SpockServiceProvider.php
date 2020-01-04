021b2871a2ae24922bfd0f0dcabeced6aaef92e1
Gaurav Punjabi <gaurav.bpunjabi@gmail.com>
2020-01-05 00:37:14 +0530

    Initial Commit

---
+++ src/SpockServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FluidTech\Spock;
+
+use Illuminate\Support\ServiceProvider;
+
+class SpockServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
